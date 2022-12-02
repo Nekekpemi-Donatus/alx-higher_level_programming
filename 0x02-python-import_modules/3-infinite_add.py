0b902c274b3e8722aa0379b6f9dc5de2c17084a4
Nekekpemi-Donatus <donatus.nekekpemi@gmail.com>
2022-12-02 21:21:56 +0100

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+if __name__ == '__main__':
+    from sys import argv
+    sum_of_arg = 0
+    arg_int = []
+    if len(argv) == 1:
+        print("{}".format(len(argv)-1))
+    elif len(argv) >= 2:
+        for arg in range(1, len(argv)):
+            arg_int.append(int(argv[arg]))
+        sum_of_arg = sum(arg_int)
+        print("{}".format(sum_of_arg))
